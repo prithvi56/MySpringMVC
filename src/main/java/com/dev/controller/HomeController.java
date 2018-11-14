f1f5d2ea44384ba86ca8bd158c8ffa5b22387d1f
patil <patil@LAPTOP-4A31RI6G>
2018-11-14 12:33:50 +0530

    2nd commit

--- src/main/java/com/dev/controller/HomeController.java
+++ src/main/java/com/dev/controller/HomeController.java
@@ -1,5 +1,16 @@
 package com.dev.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("/controller")
+public class HomeController {
+	
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String hello() {
+		return "Home";
+	}
+	
 }
